390ab1dbdf3d517a9e767289f114fbf24208aef7
Victor_Sales <israelvictords@gmail.com>
2026-02-13 21:53:59 -0400

    up

--- Hello World/3 - Tipos Customizados/Construtores/Construtores/Construtores/Program.cs
+++ Hello World/3 - Tipos Customizados/Construtores/Construtores/Construtores/Program.cs
@@ -8,8 +8,29 @@ namespace Construtores
 {
     internal class Program
     {
+        struct MeusDados
+        {
+            public string nome;
+            public int idade;
+            public float peso;
+            public float altura;
+            // Os construtores são usados para facilitar a criação de structs
+            // Os construtores devem estar dentro das Structs
+            public MeusDados(string nome, int idade, float peso, float altura)
+            {
+                this.nome = nome;
+                this.idade = idade;
+                this.peso = peso;
+                this.altura = altura;
+            }
+        }
         static void Main(string[] args)
         {
+            MeusDados Victor = new MeusDados("Israel Victor Dias Sales", 22, 110f, 1.90f);
+            MeusDados Adriana = new MeusDados("Adriana Celly Da Silva Viana", 39, 70f, 1.60f);
+            MeusDados Fabiola = new MeusDados("Fabiola Sales De Castro", 39, 84.5f, 1.74f);
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
